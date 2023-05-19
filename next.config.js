0ecd9fe48eaa0f58e258e1bdec1d86614e8f4088
Youssef Aly <youssefaly43@gmail.com>
2023-05-19 15:29:23 +0200

    fix(app): Fixed build issues.

--- next.config.js
+++ next.config.js
@@ -4,16 +4,7 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ["localhost", "picsum.photos"],
-  },
-  theme: {
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-    },
+    domains: ['localhost', 'picsum.photos'],
   },
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
   },
   transitionProperty: {
-    transform: "transform",
+    transform: 'transform',
   },
   plugins: [
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/forms"),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
   ],
 };
